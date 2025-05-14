d78eaaefb488c232932dc56078bf503ae1efd49c
makzs <erikmakiak@gmail.com>
2025-05-14 16:53:15 -0300

    Add project files.

---
+++ BlazorTest/Clients/GamesClient.cs
@@ -0,0 +1,33 @@
+﻿using BlazorTest.Models;
+
+namespace BlazorTest.Clients;
+
+public class GamesClient
+{
+    private readonly List<GameSummary> games =
+        [
+            new(){
+            Id = 1,
+            Name = "Street Figther 2",
+            Genre = "Fighting",
+            Price = 19.99M,
+            ReleaseDate = new DateOnly(1992, 7, 15)
+            },
+            new(){
+                Id = 2,
+                Name = "Final Fantasy XIV",
+                Genre = "RPG",
+                Price = 59.99M,
+                ReleaseDate = new DateOnly(2010, 9, 30)
+            },
+            new(){
+                Id = 3,
+                Name = "FIFA 23",
+                Genre = "Sports",
+                Price = 69.99M,
+                ReleaseDate = new DateOnly(2022, 9, 27)
+            },
+        ];
+
+    public GameSummary[] GetGames() => [.. games];
+}
---
+++ BlazorTest/Clients/GenreClient.cs
@@ -0,0 +1,34 @@
+﻿using BlazorTest.Models;
+
+namespace BlazorTest.Clients;
+
+public class GenreClient
+{
+    private readonly Genre[] genres =
+        [
+            new(){
+                Id = 1,
+                Name = "Fighting"
+            },
+            new(){
+                Id = 2,
+                Name = "RPG"
+            },
+            new(){
+                Id = 3,
+                Name = "Sports"
+            },
+            new(){
+                Id = 4,
+                Name = "Racing"
+            },
+            new(){
+                Id = 5,
+                Name = "Family"
+            },
+        ];
+
+    public Genre[] GetGenres() => genres;
+
+    
+}
