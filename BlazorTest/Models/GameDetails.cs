a35ce2edb8c105b2e1b0513ef9a91882381d356d
makzs <erikmakiak@gmail.com>
2025-05-15 13:57:44 -0300

    implmentando validação do formulario

--- BlazorTest/Models/GameDetails.cs
+++ BlazorTest/Models/GameDetails.cs
@@ -1,10 +1,17 @@
-﻿namespace BlazorTest.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorTest.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+    [Required(ErrorMessage = "The Genre filed is required")]
     public string? GenreId { get; set; }
+    [Range(1, 100)]
     public decimal Price { get; set; }
     public DateOnly ReleaseDate { get; set; }
 }
