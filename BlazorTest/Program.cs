55011d809bab1eec4fcdf3641747fd95c50fafbb
makzs <erikmakiak@gmail.com>
2025-05-15 21:21:25 -0300

    adicionando interatividade

--- BlazorTest/Program.cs
+++ BlazorTest/Program.cs
@@ -3,7 +3,6 @@ using BlazorTest.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddSingleton<GamesClient>();
@@ -11,11 +10,9 @@ builder.Services.AddSingleton<GenreClient>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
